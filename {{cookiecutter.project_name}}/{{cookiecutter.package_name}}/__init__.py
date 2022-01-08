0f0ef21202595dc9fa7e518a3756e5c64b531382
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2022-01-08 08:42:56 -0800

    fixup! Add docs to outer CI

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,5 +1,9 @@
 """{{cookiecutter.friendly_name}}."""
 
+__author__ = """{{ cookiecutter.author }}"""
+__email__ = "{{ cookiecutter.email }}"
+__version__ = "{{ cookiecutter.version }}"
+
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
 from logging import NullHandler
